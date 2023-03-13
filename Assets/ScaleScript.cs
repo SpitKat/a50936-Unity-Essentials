db04303809bcf3142ff159a53a99076f52a87f96
SpitKat <126791893+SpitKat@users.noreply.github.com>
2023-03-13 14:06:51 +0000

    não dá

--- Assets/ScaleScript.cs
+++ Assets/ScaleScript.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class ScaleScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        public Vector3 scaleChange;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.localScale += scaleChange;
     }
 }
