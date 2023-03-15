d425a3c327e95e5abb69061666ef84c87b8ef19c
SpitKat <126791893+SpitKat@users.noreply.github.com>
2023-03-15 14:47:53 +0000

    outdated

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,19 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
 
-public class ScaleScript : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        public Vector3 scaleChange;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.localScale += scaleChange;
+        
     }
 }
